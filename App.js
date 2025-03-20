29b8b4d52a1e396b8bc2a8b18c3b16c440bfb68c
DaviLopesDeCarvalho <davilopes1014@gmail.com>
2025-03-20 18:09:39 -0300

    Alterando Atividade 1 e Realzando Atividade 2

--- App.js
+++ App.js
@@ -1,92 +1,126 @@
-import React, { Component } from "react";
-import { View, Text, StyleSheet, SectionList, StatusBar, Image } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
+import { useFonts } from "expo-font";
+import { Quicksand_300Light, Quicksand_600SemiBold, Quicksand_700Bold } from "@expo-google-fonts/quicksand";
 
-const DATA = [
-  {
-    title: 'Moda & Acessórios',
-    data: [
-      { title: 'Roupas masculinas', image: require('./assets/roupas_masculinas.png') },
-      { title: 'Roupas femininas', image: require('./assets/roupas_femininas.png') },
-      { title: 'Óculos e acessórios', image: require('./assets/oculos.png') },
-      { title: 'Calçados', image: require('./assets/calcados.png') },
-      { title: 'Bolsas e mochilas', image: require('./assets/bolsas.png') },
-    ],
-  },
-  {
-    title: 'Eletrônicos & Tecnologia',
-    data: [
-      { title: 'Celulares e tablets', image: require('./assets/celulares.png') },
-      { title: 'Computadores e acessórios', image: require('./assets/computadores.png') },
-      { title: 'Video games e consoles', image: require('./assets/videogames.png') },
-    ],
-  },
-];
 
-class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <SectionList
-          sections={DATA}
-          keyExtractor={(item, index) => item.title + index}
-          renderItem={({ item }) => (
-            <View style={styles.item}>
-              <Image source={item.image} style={styles.image} />
-              <Text style={styles.title}>{item.title}</Text>
-            </View>
-          )}
-          renderSectionHeader={({ section: { title } }) => (
-            <Text style={styles.header}>{title}</Text>
-          )}
+const Card = ({ titulo, descricao, imagem }) => {
+  return (
+    <View style={styles.card}>
+      <View style={styles.cardContent}>
+        <Image source={imagem} style={styles.imagem} />
+        <View style={styles.textContainer}>
+          <Text style={styles.titulo}>{titulo}</Text>
+          <Text style={styles.descricao}>{descricao}</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Quicksand_300Light,
+    Quicksand_700Bold,
+    Quicksand_600SemiBold
+  });
+
+  return (
+    
+    <ScrollView>
+      <View style={styles.bordacard}>
+        <Text style={styles.header}>Moda & Acessórios</Text>
+        <Card
+          titulo="Roupas Masculinas"
+          descricao="Camisetas, calças, jaquetas..."
+          imagem={require('./assets/img/roupas_masculinas.png')}
+        />
+
+        <Card
+          titulo="Roupas Femininas"
+          descricao={"Vestidos, blusas, saias..."}
+          imagem={require('./assets/img/roupas_femininas.png')}
+        />
+
+        <Card
+          titulo={"Óculos e Acessórios"}
+          descricao={"óculos de sol, relógios, bijuterias..."}
+          imagem={require('./assets/img/oculos.png')}
+        />
+        <Card
+          titulo={"Calçados"}
+          descricao={"Tênis, botas, sandálias, sapatos..."}
+          imagem={require('./assets/img/calcados.png')}
+        />
+
+        <Text style={styles.header}>Eletrônicos & Tecnologia</Text>
+        <Card
+          titulo={"Celulares e Tablets"}
+          descricao={"smartphones e tablets usados."}
+          imagem={require('./assets/img/celulares.png')}
+        />
+        <Card
+          titulo={"Computadores e Acessórios"}
+          descricao={"Notebooks, monitores, periféricos..."}
+          imagem={require('./assets/img/computadores.png')}
+        />
+        <Card
+          titulo={"Video Games e Consoles"}
+          descricao={"Jogos, consoles e acessórios."}
+          imagem={require('./assets/img/videogames.png')}
         />
       </View>
-    );
-  }
+    </ScrollView>
+  );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: StatusBar.currentHeight,
-    alignItems: 'center',
-    
-  },
-  item: {
+  card: {
+    padding: 15,
+    borderRadius: 10,
     backgroundColor: '#eaeaea',
-    borderRadius: 8,
-    borderColor:'#ffffff',
-    marginTop: 15,
-    width: 350, 
-    height: 250,
-    marginBottom: 20,
-    alignItems: 'center',
+    shadowOffset: { width: 0, height: 3 },
+    marginBottom: 15,
+    width: 350,
+    elevation: 7,
   },
-  image: {
-    width: 150, 
-    height: 150,
-    borderRadius: 8,
-    marginTop: 20,
-    marginBottom: 20,
-    alignItems: 'center',
-    
+  cardContent: {
+    flexDirection: 'row', 
+    alignItems: 'center', 
+  },
+  imagem: {
+    width: 100, 
+    height: 100, 
+    borderRadius: 10,
+    marginRight: 10,
   },
   header: {
-    fontFamily: 'Quicksand',
+    fontFamily: 'Quicksand_700Bold',
+    marginTop: 15,
+    marginBottom: 15,
     marginLeft: 15,
     textAlign: 'center',
-    fontSize: 32,
-    fontWeight: 300,
-    fontStyle: 'normal',
+    fontSize: 18,
     backgroundColor: '#fff',
   },
-  title: {
-    fontFamily: 'Quicksand',
-    fontFamily: 'sans-serif',
-    fontSize: 24,
-    fontWeight: 100,
-    fontStyle: 'normal',
-    textAlign: 'center',
+  titulo: {
+    fontFamily: 'Quicksand_700Bold',
+    fontSize: 16,
+    textAlign: 'left',
+    justifyContent: 'left',
+  },
+  descricao: {
+    marginTop: 8,
+    fontSize: 12,
+    textAlign: 'left',
+    justifyContent: 'left',
+    fontFamily: 'Quicksand_600SemiBold',
+  },
+  bordacard: {
+    flex: 1,
+    backgroundColor: '#ffff',
+    padding: 10,
+    alignItems: 'center',
   },
 });
-
-export default App;
